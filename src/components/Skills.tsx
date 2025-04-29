604ba4701da8286fdcf73cb982c8a22d2f631504
Sajith Minrutha <minruthasajith@gmail.com>
2025-04-29 15:51:50 -0600

    create basic skills section loop

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,81 +1,53 @@
 function Skills() {
+  const ListOfLanguages: string[] = [
+    "REACT JS",
+    "JAVASCRIPT",
+    "TYPESCRIPT",
+    "GIT",
+    "JAVA",
+    "Python",
+    "DJANGO",
+    "PYTHON",
+    "C++",
+    "SOLDITY",
+    "MYSQL",
+    "MONGODB",
+    "FIREBASE",
+    "EXPRESS JS",
+    "NODE JS",
+    "ZUSTAND",
+    "HTML",
+    "CSS",
+  ];
+  const ListOfImages: string[] = [
+    "/images/react.png",
+    "/images/javascript.png",
+    "/images/typescript.png",
+    "/images/git.png",
+    "/images/java.png",
+    "/images/python.png",
+    "/images/django.png",
+    "/images/python.png",
+    "/images/c++.png",
+    "/images/solidity.png",
+    "/images/mysql.png",
+    "/images/mongodb.png",
+    "/images/firebase.png",
+    "/images/express.png",
+    "/images/nodejs.png",
+    "/images/zustand.png",
+    "/images/html.png",
+    "/images/css.png",
+  ];
+
   return (
     <>
       <div className="default-flex flex-col my-5">
         <h1 className="text-3xl font-mono">Skills</h1>
         <div className="grid grid-cols-4 gap-4 mt-5 flex-grow">
-          <div>
-            <image />
-            <h3>REACT JS</h3>
-          </div>
-          <div>
-            <image />
-            <h3>JAVASCRIPT</h3>
-          </div>
-          <div>
-            <image />
-            <h3>TYPESCRIPT</h3>
-          </div>
-          <div>
-            <image />
-            <h3>GIT</h3>
-          </div>
-          <div>
-            <image />
-            <h3>JAVA</h3>
-          </div>
-          <div>
-            <image />
-            <h3>Python</h3>
-          </div>
-          <div>
-            <image />
-            <h3>DJANGO</h3>
-          </div>
-          <div>
-            <image />
-            <h3>PYTHON</h3>
-          </div>
-          <div>
-            <image />
-            <h3>C++</h3>
-          </div>
-          <div>
-            <image />
-            <h3>SOLDITY</h3>
-          </div>
-          <div>
-            <image />
-            <h3>MYSQL</h3>
-          </div>
-          <div>
-            <image />
-            <h3>MONGODB</h3>
-          </div>
-          <div>
-            <image />
-            <h3>FIREBASE</h3>
-          </div>
-          <div>
-            <image />
-            <h3>EXPRESS JS</h3>
-          </div>
-          <div>
-            <image />
-            <h3>NODE JS</h3>
-          </div>
-          <div>
-            <image />
-            <h3>ZUSTAND</h3>
-          </div>
-          <div>
-            <image />
-            <h3>HTML</h3>
-          </div>
-          <div>
-            <image />
-            <h3>CSS</h3>
-          </div>
+          {ListOfLanguages.map((element) => {
+            return <h3>{element}</h3>;
+          })}
         </div>
       </div>
     </>
