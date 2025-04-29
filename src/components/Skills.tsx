d03604bb575c9e8880149cf5c4fe647efe0f6327
Sajith Minrutha <minruthasajith@gmail.com>
2025-04-29 17:16:05 -0600

    create basic skills section loop

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,43 +1,34 @@
+import Image from "next/image";
+
 function Skills() {
-  const ListOfLanguages: string[] = [
-    "REACT JS",
-    "JAVASCRIPT",
-    "TYPESCRIPT",
-    "GIT",
-    "JAVA",
-    "Python",
-    "DJANGO",
-    "PYTHON",
-    "C++",
-    "SOLDITY",
-    "MYSQL",
-    "MONGODB",
-    "FIREBASE",
-    "EXPRESS JS",
-    "NODE JS",
-    "ZUSTAND",
-    "HTML",
-    "CSS",
-  ];
-  const ListOfImages: string[] = [
-    "/images/react.png",
-    "/images/javascript.png",
-    "/images/typescript.png",
-    "/images/git.png",
-    "/images/java.png",
-    "/images/python.png",
-    "/images/django.png",
-    "/images/python.png",
-    "/images/c++.png",
-    "/images/solidity.png",
-    "/images/mysql.png",
-    "/images/mongodb.png",
-    "/images/firebase.png",
-    "/images/express.png",
-    "/images/nodejs.png",
-    "/images/zustand.png",
-    "/images/html.png",
-    "/images/css.png",
+  type Skill = {
+    name: string;
+    image: string;
+  };
+  const skills: Skill[] = [
+    { name: "REACT JS", image: "/react-original.svg" },
+    { name: "JAVASCRIPT", image: "/javascript-original.svg" },
+    { name: "TYPESCRIPT", image: "/typescript-original.svg" },
+    { name: "GIT", image: "/git-original.svg" },
+    { name: "JAVA", image: "/java-original.svg" },
+    { name: "PYTHON", image: "/python-original.svg" },
+    { name: "DJANGO", image: "/django-original.svg" },
+    { name: "C++", image: "/c-original.svg" },
+    { name: "SOLIDITY", image: "/solidity.svg" },
+    { name: "MYSQL", image: "/mysql-original.svg" },
+    { name: "MONGODB", image: "/mongodb-original.svg" },
+    { name: "FIREBASE", image: "/firebase-original.svg" },
+    { name: "EXPRESS JS", image: "/express-original.svg" },
+    { name: "NODE JS", image: "/nodejs-original.svg" },
+    { name: "ZUSTAND", image: "/zustand-original.svg" },
+    { name: "HTML", image: "/html5-original.svg" },
+    { name: "CSS", image: "/css3-original.svg" },
+    { name: "TAILWIND CSS", image: "/tailwindcss-original.svg" },
+    { name: "BOOTSTRAP", image: "/bootstrap-original.svg" },
+    { name: "SASS", image: "/sass-original.svg" },
+    { name: "NEXT JS", image: "/nextjs-original.svg" },
+    { name: "REDUX", image: "/redux-original.svg" },
+    { name: "GRAPHQL", image: "/graphql-original.svg" },
   ];
 
   return (
@@ -45,8 +36,16 @@ function Skills() {
       <div className="default-flex flex-col my-5">
         <h1 className="text-3xl font-mono">Skills</h1>
         <div className="grid grid-cols-4 gap-4 mt-5 flex-grow">
-          {ListOfLanguages.map((element) => {
-            return <h3>{element}</h3>;
+          {skills.map((skill, index) => {
+            return (
+              <div
+                key={index}
+                className="flex flex-col justify-center items-center gap-5 w-50  mt-10"
+              >
+                <Image src={skill.image} width={80} height={80} alt="image" />
+                <h3>{skill.name}</h3>
+              </div>
+            );
           })}
         </div>
       </div>
