e7249ba69a10ba0d41e62f78dcc0629c42fa90ac
Sajith Minrutha <minruthasajith@gmail.com>
2025-04-28 22:20:16 -0600

    create basic landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
+import AboutMe from "@/components/AboutMe";
 import LandingPage from "../components/LandingPage";
 import NavBar from "../components/NavBar";
+import Education from "@/components/Education";
 export default function Home() {
   return (
     <main>
       <NavBar />
       <LandingPage />
+      <AboutMe />
+      <Education />
     </main>
   );
 }
---
+++ src/components/AboutMe.tsx
@@ -0,0 +1,23 @@
+function AboutMe() {
+  return (
+    <div className="default-flex flex-col  my-5 ">
+      <h2 className="text-3xl font-mono">About Me</h2>
+      <p className="font-mono my-5 w-1/2 text-left">
+        I’m a skilled and adaptable Software Engineer with experience building
+        useful and creative solutions in areas like education, healthcare, and
+        social good projects. I’m strong in full-stack development, software
+        testing, and project planning, and I enjoy using new technologies to
+        solve real-world problems.
+        <br />
+        <br />
+        I’ve worked well with different teams, helped improve product quality,
+        and made sure projects meet users’ needs. I know how to build scalable
+        web apps, connect systems using APIs, and add features that people find
+        useful. I’m always learning and staying up to date with the latest tools
+        and trends in technology. I’m passionate about using my skills to create
+        positive change and contribute to meaningful projects that help people.
+      </p>
+    </div>
+  );
+}
+export default AboutMe;
